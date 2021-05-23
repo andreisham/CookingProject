a8f72c6fef255a9d61a026ba35d244cdaee9242e
Yaroslav Bemov <yaroslav.bemov@gmail.com>
2021-05-23 16:21:18 +0300

    add idea to ignore

---
+++ app/Http/Controllers/IngredientController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Ingredient;
+use Illuminate\Http\Request;
+
+class IngredientController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $ingredients = Ingredient::all();
+
+        return response($ingredients, 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Ingredient  $ingredient
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Ingredient $ingredient)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Ingredient  $ingredient
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Ingredient $ingredient)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Ingredient  $ingredient
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Ingredient $ingredient)
+    {
+        //
+    }
+}
---
+++ app/Models/Ingredient.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Ingredient extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description'
+    ];
+
+    protected $hidden = [
+        'created_at',
+        'updated_at'
+    ];
+
+//    public function meals() {
+//        return $this->hasMany(Meal::class);
+//    }
+}
