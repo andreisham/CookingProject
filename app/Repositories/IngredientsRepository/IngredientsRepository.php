cb04493805f592935c7f67ed5571efc072f5305e
Evgeny Kuzmin <skchel174@gmail.com>
2021-05-28 14:12:32 +0500

    Add IngredientsRepository

---
+++ app/Repositories/IngredientsRepository/IngredientsRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Repositories\IngredientsRepository;
+
+use App\Models\Ingredient;
+
+class IngredientsRepository implements IngredientsRepositoryInterface
+{
+
+    public function getAll(): array
+    {
+        return Ingredient::get()->toArray();
+    }
+}
