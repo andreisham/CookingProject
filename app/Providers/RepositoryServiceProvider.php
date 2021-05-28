77fcf2884c8e24df1d4cbb4c0d3fd053f3bcb269
Evgeny Kuzmin <skchel174@gmail.com>
2021-05-28 14:20:55 +0500

    Update IngredientController

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Repositories\IngredientsRepository\IngredientsRepository;
+use App\Repositories\IngredientsRepository\IngredientsRepositoryInterface;
 use App\Repositories\MealsRepository\MealsRepository;
 use App\Repositories\MealsRepository\MealsRepositoryInterface;
 use Illuminate\Support\ServiceProvider;
@@ -11,5 +13,6 @@ class RepositoryServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->bind(MealsRepositoryInterface::class, MealsRepository::class);
+        $this->app->bind(IngredientsRepositoryInterface::class, IngredientsRepository::class);
     }
 }
--- app/Repositories/IngredientsRepository/IngredientsRepository.php
+++ app/Repositories/IngredientsRepository/IngredientsRepository.php
@@ -6,7 +6,6 @@ use App\Models\Ingredient;
 
 class IngredientsRepository implements IngredientsRepositoryInterface
 {
-
     public function getAll(): array
     {
         return Ingredient::get()->toArray();
